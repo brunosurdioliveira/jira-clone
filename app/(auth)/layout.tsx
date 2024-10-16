a03f0f5c6b862bdde496c5394f5a81de80176e17
Bruno Surdi <bruno.surdi.oliveira@gmail.com>
2024-10-16 15:51:49 -0400

    Hono integration / shadcn / tanstack query / sign in page / sign up page
    - routes set up
    - components importing from shadcn
    - First integration of front end and backend using query
    - Sign in and Sign page ui / integration with api

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+interface AuthLayoutProps {
+    children: React.ReactNode;
+}
+
+const SignInLayout = ({children} : AuthLayoutProps) => {
+    const pathname = usePathname();
+    return (
+        <main className="bg-neutral-100 min-h-screen">
+            <div className="mx-auto max-w-screen-2xl p-4">
+                <nav className="flex justify-between items-center">
+                    <Image src="/logo.svg" height={56} width={152} alt="logo" />
+                    <Button asChild variant="secondary">
+                        <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+                            {pathname === "/sign-in" ? "Sign Up" : "Login" }
+                        </Link>
+                    </Button>
+                </nav>
+            </div>
+            <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+                {children}
+            </div>
+        </main>
+    );
+}
+
+export default SignInLayout;
