7fb9c71023d4f4c731b9eecb582c960cfba5e2b4
Bruno Surdi <bruno.surdi.oliveira@gmail.com>
2024-10-18 16:43:54 -0400

    Middleware / login / logout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
+        suppressHydrationWarning
         className={cn(inter.className, "antialiased min-h-screen")}
       >
         <QueryProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,24 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data])
+
   return (
-    <div className="">
-      <Input />
-      <Button size="lg">
-        Primary
-      </Button>
-      <Button variant="secondary">
-        Secondary
-      </Button>
-      <Button variant="destructive">
-        Destructive
-      </Button>
-      <Button variant="ghost">
-        Ghost
-      </Button>
-      <Button variant="muted">
-        Muted
-      </Button>
-      <Button variant="outline">
-        Outline
-      </Button>
-      <Button variant="teritary">
-        Teritary
-      </Button>
+    <div>
+      <UserButton />
     </div>
   );
 }
